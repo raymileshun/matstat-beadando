1f4edaed97b7e16f20864ea9af021d0ea75db4e6
horakrajmund <Rajmund.Horak@wup.hu>
2020-02-28 16:00:21 +0100

    Normalis eloszles eses szimulacio hozzaadas. Meg nem teljesen tokeletes

---
+++ eses_szimulacio/sketch.js
@@ -0,0 +1,92 @@
+var Engine = Matter.Engine,
+    //Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+var engine;
+var world;
+var points=[];
+let ground, leftWall, rightWall;
+let sdLine, sdConstrainLeft,sdConstrainRight,offset=40;
+let sdLineLength=300,sdLineHeightPos=100;
+let separators=[];
+let separatorCount=10;
+let pointRadius=12;
+let visible=true
+
+function setup() {
+  createCanvas(500, 500);
+  rectMode(CENTER)
+
+  engine=Engine.create()
+  world=engine.world;
+  Engine.run(engine)
+  ground=Bodies.rectangle(width/2,height,width,20,{isStatic:true})
+  leftWall=Bodies.rectangle(0,height,1,2*height,{isStatic:true})
+  rightWall=Bodies.rectangle(width,height,1,2*height,{isStatic:true})
+  World.add(world,[ground,leftWall,rightWall])
+
+  sdLine=Bodies.rectangle((width/2),sdLineHeightPos,sdLineLength,2,{isStatic:true})
+  sdConstrainLeft=Bodies.rectangle(sdLine.bounds.min.x,sdLineHeightPos/2-offset,2,sdLineHeightPos+2*offset,{isStatic:true})
+  sdConstrainRight=Bodies.rectangle(sdLine.bounds.max.x,sdLineHeightPos/2-offset,2,sdLineHeightPos+2*offset,{isStatic:true})
+  World.add(world,[sdLine,sdConstrainLeft,sdConstrainRight])
+  console.log(sdConstrainLeft)
+
+
+  for(let i=0;i<separatorCount;i++){
+    separators.push(Bodies.rectangle((0+i*width/separatorCount),height,4,height,{isStatic:true}))
+    World.add(world,separators[i]);
+  }
+}
+
+
+function mouseDragged(){
+  points.push(new Circle(mouseX,mouseY,pointRadius))
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    Matter.Composite.remove(world, sdLine)
+    visible=false
+  }
+}
+
+function drawPointsContainer(){
+  if(visible){
+    push()
+    fill(0)
+    strokeWeight(2);
+    line(sdLine.bounds.min.x,sdLine.bounds.min.y,sdLine.bounds.max.x,sdLine.bounds.max.y)
+    pop()
+  }
+
+  fill(0)
+  strokeWeight(2);
+  line(sdConstrainLeft.bounds.min.x,sdConstrainLeft.bounds.min.y,sdConstrainLeft.bounds.max.x,sdConstrainLeft.bounds.max.y)
+
+  fill(0)
+  strokeWeight(2);
+  line(sdConstrainRight.bounds.min.x,sdConstrainRight.bounds.min.y,sdConstrainRight.bounds.max.x,sdConstrainRight.bounds.max.y)
+}
+
+function draw() {
+  background(60)
+  drawPointsContainer();
+
+
+
+  for(let i=0;i<points.length;i++){
+    points[i].show();
+  }
+
+  push()
+  fill(0)
+  strokeWeight(15);
+  line(0,height,width,height)
+  pop()
+
+  for(let i=0;i<separators.length;i++){
+    line(separators[i].position.x,separators[i].position.y,separators[i].position.x,separators[i].bounds.min.y);
+  }
+
+}
